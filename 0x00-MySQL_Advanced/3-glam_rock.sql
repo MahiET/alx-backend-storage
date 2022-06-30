48496a3036b83bedd9e16c18811f58978338e6f3
MahiET <meazalemma2020@gmail.com>
2022-06-30 14:30:18 +0000

    Old School

--- 0x00-MySQL_Advanced/3-glam_rock.sql
+++ 0x00-MySQL_Advanced/3-glam_rock.sql
@@ -4,5 +4,5 @@
 
 SELECT band_name, ifnull(split, 2020)-ifnull(formed, 0) AS lifespan
 FROM metal_bands
-WHERE style LIKE "%Glam rock%"
-ORDER BY lifespan DESC;
+WHERE style LIKE '%Glam rock%'
+ORDER BY lifespan DESC
