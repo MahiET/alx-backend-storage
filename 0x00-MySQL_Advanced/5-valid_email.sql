252417a38da511819967b6066bc908b7a3c73057
MahiET <meazalemma2020@gmail.com>
2022-06-30 15:07:48 +0000

    Task 5

--- 0x00-MySQL_Advanced/5-valid_email.sql
+++ 0x00-MySQL_Advanced/5-valid_email.sql
@@ -1,14 +1,16 @@
--- Initial
-DROP TABLE IF EXISTS users;
-
-CREATE TABLE IF NOT EXISTS users (
-    id int not null AUTO_INCREMENT,
-    email varchar(255) not null,
-    name varchar(255),
-    valid_email boolean not null default 0,
-    PRIMARY KEY (id)
-);
-
-INSERT INTO users (email, name) VALUES ("bob@dylan.com", "Bob");
-INSERT INTO users (email, name, valid_email) VALUES ("sylvie@dylan.com", "Sylvie", 1);
-INSERT INTO users (email, name, valid_email) VALUES ("jeanne@dylan.com", "Jeanne", 1);
+-- Creates a trigger that resets the attribute valid_email
+-- only when the email has been changed.
+-- @author Meaza Lemma <https://github.com/MahiET>
+DROP TRIGGER IF EXISTS validate_email;
+DELIMITER $$
+CREATE TRIGGER validate_email
+BEFORE UPDATE ON users
+FOR EACH ROW
+BEGIN
+    IF OLD.email != NEW.email THEN
+        SET NEW.valid_email = 0;
+    ELSE
+        SET NEW.valid_email = NEW.valid_email;
+    END IF;
+END $$
+DELIMITER ;
